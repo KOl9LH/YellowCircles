446472f12a25ed41aa244c9b0a9b835fe5bf4626
Николай Резвушкин <nickrezvuskin@yandex.ru>
2024-12-08 18:40:27 +0300

    слегка поменял решение второй задачи из классной работы тк неправильно прочитал задание

--- main.py
+++ main.py
@@ -1,57 +1,33 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication
-from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtGui import QPainter, QColor
-import random
 import sys
+import random
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.uic import loadUi
 
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
-        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(310, 500, 151, 41))
-        self.pushButton.setObjectName("pushButton")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "yellow circle"))
-        self.pushButton.setText(_translate("MainWindow", "Кнопка"))
-class YellowCircle(QMainWindow, Ui_MainWindow):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.SpawnYellowCircle)
-
-        self.circles = []
-
-
-    def SpawnYellowCircle(self):
-        x = random.randint(0, self.width() - 50)
-        y = random.randint(0, self.height() - 50)
-        diameter = random.randint(20, 100)
-        self.circles.append((x, y, diameter))
-        self.update()
+        super(MainWindow, self).__init__()
+        loadUi("UI.ui", self)
+        self.pushButton.clicked.connect(self.add_circle)
 
     def paintEvent(self, event):
-        qp = QPainter(self)
-        for (x, y, diameter) in self.circles:
-            qp.setBrush(QColor(255, 255, 0))
-            qp.drawEllipse(x, y, diameter, diameter)
+        painter = QtGui.QPainter(self)
+        for circle in self.circles:
+            painter.setBrush(QtGui.QColor(255, 255, 0))
+            painter.drawEllipse(circle)
+
+    def add_circle(self):
+        diameter = random.randint(20, 100)
+        x = random.randint(0, self.width() - diameter)
+        y = random.randint(0, self.height() - diameter)
+        circle = QtCore.QRect(x, y, diameter, diameter)
+        self.circles.append(circle)
+        self.update()
 
 
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = YellowCircle()
-    ex.show()
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.circles = []
+    window.show()
     sys.exit(app.exec())
